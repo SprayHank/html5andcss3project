0ea8ddb55ff4aafe7522346b56dec74d9e0418e3
SprayHank <fantasyoui@gmail.com>
2013-09-02 00:10:34 +0800

    index

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html><html><head><meta http-equiv="Content-Type" content="text/html; Charset=UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=EDGE,chrome=1" />
 <title>HTML5 &amp; CSS3 Project</title>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js">//CDN GreenSock JS</script>
-</head><body></body></html>
+<script src="Static/JavaScript/Modernizr.js">//Modernizr JS</script>
+<?PHP
+include 'FetchCDN/index.php';
+exportjs('TweenMax.min.js');
+?></head><body></body></html>
