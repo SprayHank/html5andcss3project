8851a02cc21469653cca7e71268ef4a8779276b9
SprayHank <fantasyoui@gmail.com>
2013-09-02 00:10:49 +0800

    FetchCDN Config Updatetime

---
+++ FetchCDN/Config.php
@@ -0,0 +1,8 @@
+<?PHP
+$UPDATETIME = array(
+	'TweenMax.min.js' => 1378051544,
+	//	array(
+	//		'http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js',
+	//		'CDN GreenSock JS'
+	//	),
+);
